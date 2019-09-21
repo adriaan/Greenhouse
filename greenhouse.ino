bd33081c98112096509ecbc8e32f2addd51c8d7a
Adriaan Stellingwerff <adriaan@fieldapps.co>
2019-09-21 14:55:00 +1000

    Add different modes, button to change modes, LEDs to indicate mode and system status. Add fixed interval misting when sensor readings are 0. Deal with millis() overflow

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,6 +1,25 @@
 
 #include "DHT.h"
 
+// LED configuration
+#define normalOperationLEDPin 3
+#define forceMistingLEDPin 4
+#define stopMistingLEDPin 5
+#define soilSensorErrorLEDPin 6
+
+// Push button configuration
+#define buttonPin 9
+
+// Push button vaiables
+
+// operation mode of the system, based on push button presses
+// 0 = normal
+// 1 = force misting
+// 2 = stop misting
+int operationMode = 0;       
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
 // Temperature / Humidity sensor configuration
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
@@ -31,22 +50,28 @@ int soilMoisture = 0; // Value for storing moisture measurement
 // Interval between soil mouisture measurements in milliseconds
 unsigned long soilMeasurementInterval = 600000;
 
+// Interval between mistings when sensor is nor working in milliseconds
+unsigned long sensorlessWaitingInterval = 43200000; // 12 hours
 
 // Interval the pump will run for in milliseconds
-unsigned long mistingInterval = 120000; 
+unsigned long mistingInterval = 120000;
 
 // If this threshold is reached, we conclude the moisture sensor is not in soil. 
 // In this case we will switch to initermittent misting at fixed intervals
 int soilMoistureSensorOutOfSoilThreshold = 0;
 
 // Threshold value for soil moisture, if soil moisture levels drop below this, we will mist.
-int soilMoistureThreshold = 200; 
+int soilMoistureMistingThreshold = 200; 
+// Threshold value for soil moisture, if soil moisture levels drop below this, we assume sensor has been removed from soil or is broken
+// In that case we switch to misting on fixed schedule
+int soilMoistureSensorErrorThreshold = 1;
 
 unsigned long mistingStartMillis = 0;
 unsigned long waitingPeriodStartMillis = 0;
 
 bool isMisting = false;
 bool isWaiting = false;
+bool soilSensingFailed = false;
 
 void setup() 
 {
@@ -58,17 +83,46 @@ void setup()
   pinMode(relaySignalPin, OUTPUT); // Set D8 as an OUTPUT
   digitalWrite(relaySignalPin, LOW); // Set to LOW so relay is switched OFF
 
+  pinMode(buttonPin, INPUT);
+  
+  // Configure LEDs
+  pinMode(normalOperationLEDPin, OUTPUT);
+  digitalWrite(normalOperationLEDPin, HIGH); // Default is normal operation
+
+  pinMode(forceMistingLEDPin, OUTPUT);
+  digitalWrite(forceMistingLEDPin, LOW); // Default is normal operation
+
+  pinMode(stopMistingLEDPin, OUTPUT);
+  digitalWrite(stopMistingLEDPin, LOW); // Default is normal operation
+
+  pinMode(soilSensorErrorLEDPin, OUTPUT); 
+  digitalWrite(soilSensorErrorLEDPin, LOW); // Default is normal operation
+
   dht.begin();
 }
 
 void loop() 
 {
   unsigned long currentMillis = millis();
+  if (currentMillis < mistingStartMillis || currentMillis < waitingPeriodStartMillis) {
+    // if millis() has overflown (happens after about 50 days), need to reset our variables
+    reset();
+  }
   mistingLoop(currentMillis);
   tempHumidityLoop(currentMillis);
+  readButtonState(currentMillis);
+}
+
+void reset() {
+  mistingStartMillis = 0;
+  waitingPeriodStartMillis = 0;
+  lastTemperatureMeasurementMillis = 0;
+  isWaiting = false;
+  isMisting = false;
 }
 
 // Temp / Humidity code
+
 void tempHumidityLoop(unsigned long currentMillis) {
   if (currentMillis > lastTemperatureMeasurementMillis + tempMeasurementInterval || lastTemperatureMeasurementMillis == 0) {
     measureTemperatureAndHumidity();
@@ -102,15 +156,79 @@ void measureTemperatureAndHumidity() {
   Serial.println(F("°C "));
 }
 
+// Operation mode code
+
+void readButtonState(unsigned long currentMillis) {
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    lastButtonState = buttonState;
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      operationMode = (operationMode + 1) % 3;
+      Serial.print("operationMode: ");
+      Serial.println(operationMode);
+      updateToNewMode(currentMillis);
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+}
+
+void updateToNewMode(unsigned long currentMillis) {
+   resetLEDs();
+   if (operationMode == 0) { // normal
+      digitalWrite(normalOperationLEDPin, HIGH);
+      // Reset so that misting loop sample soil moisture and goes back to normal from there.
+      stopMisting();
+      isWaiting = false;
+   } else if (operationMode == 1) { // force misting
+      startMisting(currentMillis);
+      digitalWrite(forceMistingLEDPin, HIGH);
+   } else if (operationMode == 2) { // stop misting
+      stopMisting(); 
+      digitalWrite(stopMistingLEDPin, HIGH);
+   }
+}
+
+void updateErrorLED() {
+  if(soilSensingFailed) {
+    digitalWrite(soilSensorErrorLEDPin, HIGH);
+  } else {
+    digitalWrite(soilSensorErrorLEDPin, LOW);
+  }
+}
+
+void resetLEDs() {
+  digitalWrite(normalOperationLEDPin, LOW);
+  digitalWrite(forceMistingLEDPin, LOW);
+  digitalWrite(stopMistingLEDPin, LOW);
+}
+
 // Misting code
+
 void mistingLoop(unsigned long currentMillis) {
+   // Only execture misting loop if we are in normal operation mode
+   if (operationMode != 0){ return; }
    if (isWaiting == false && isMisting == false) {
      soilMoisture = readSoil();
-     if (soilMoisture < soilMoistureThreshold) {
+     if (soilMoisture <= soilMoistureSensorErrorThreshold) {
+        soilSensingFailed = true;
+        // If we can't get a proper reading from the soil sensor, mist according to the fixed schedule
+        if (currentMillis > mistingStartMillis + sensorlessWaitingInterval) {
+            startMisting(currentMillis);
+        } else {
+            startWaiting(currentMillis);
+        }
+     } else if (soilMoisture < soilMoistureMistingThreshold) {
+        soilSensingFailed = false;
         startMisting(currentMillis);
      } else {
+        soilSensingFailed = false;
         startWaiting(currentMillis);
      }
+     updateErrorLED();
   } else if (isWaiting == true) {
     if (currentMillis > waitingPeriodStartMillis + soilMeasurementInterval) {
         stopWaiting();
